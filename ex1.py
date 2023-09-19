2444b5ef9fa1a70c963f35bb1535e4bac6029ef6
gmom-cyber <70742226+gmom-cyber@users.noreply.github.com>
2023-09-19 18:37:53 +0300

    Финал ex1.py
    Добавлены комментарии

--- ex1.py
+++ ex1.py
@@ -1,14 +1,14 @@
 import requests, hashlib
-file1 = open("sample_data/data.txt", "r")
+file1 = open("data.txt", "r")#файл и код должны лежать в одной папке
 lines = file1.readlines()
 for line in lines:
-   sha1_hash = hashlib.sha1((line.strip()[line.strip().find(',')+1:]).encode())#f231f38cd872faae6a136cfa62d7be4b5728e668
-   sha1_hex = sha1_hash.hexdigest()
-   req = requests.get('https://api.pwnedpasswords.com/range/'+sha1_hex[:5])
+   sha1_hash = hashlib.sha1((line.strip()[line.strip().find(',')+1:]).encode())#создание хеша пароля
+   sha1_hex = sha1_hash.hexdigest()#перевод хеша 
+   req = requests.get('https://api.pwnedpasswords.com/range/'+sha1_hex[:5])#запрос на сервис
    reqStr = str(req.content.decode('ascii')).replace('\r','')#cтрока состоящая из хеша и количества совпадений
-   if reqStr.find(sha1_hex[5:].upper()) == -1:
+   if reqStr.find(sha1_hex[5:].upper()) == -1:#если подстрока не найдено выводим сообщение об этом
     print("утечки пароля у пользователя " + (line.strip()[:line.strip().find(',')]) + " не обнаружено\n")
-   #print(reqStr)#F7478020253F747B6F9BAF48D571471B18C:377
+  
    else:
     print("утечка пароля у пользователя " + (line.strip()[:line.strip().find(',')]))
 
